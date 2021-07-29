7a491bbaea6c9726aa51ccd1a43958c4ccff0f0b
Francesco Morri <francesco98m@gmail.com>
2021-07-29 12:14:47 +0200

    game of life improved

--- projects/game-of-life/sketch.js
+++ projects/game-of-life/sketch.js
@@ -8,7 +8,7 @@ var sidex, sidey;
 function setup() {
   cnv = createCanvas(800, 500);
   cnv.parent("container");
-  cnv.mousePressed(random_start);
+  cnv.mousePressed(create_config);
 
   sidex = width / scl;
   sidey = height / scl;
@@ -82,7 +82,7 @@ function draw() {
   }
 }
 
-function random_start(){
+function create_config(){
   var x = floor(mouseX/scl);
   var y = floor(mouseY/scl);
 
@@ -99,4 +99,79 @@ function random_start(){
   else if(mouseButton === RIGHT){
     start = !start;
   }
+}
+
+
+function keyPressed(){
+  if (key == ' '){
+    start = !start;
+  }
+  if (key == '0'){
+    for(let i=0; i<sidex; i++){
+      for(let j=0; j<sidey; j++){
+        let r = random();
+        if(r<0.5){
+          world[i][j] = 1;
+        } else {
+          world[i][j] = 0;
+        }
+      }
+    }
+  } else if (key == '1') {
+    create_configurations(1);
+  }
+}
+
+function create_configurations(config){
+  //clean map
+  for (let i = 0; i < sidex; i++) {
+    for (let j = 0; j < sidey; j++) {
+      world[i][j] = 0;
+    }
+  }
+  if(config == 1){
+    //Gosper gun
+    let x = 7;
+    let y = 7;
+    world[x][y] = 1;
+    world[x+1][y] = 1;
+    world[x][y+1] = 1;
+    world[x+1][y+1] = 1;
+
+    world[x+10][y] = 1;
+    world[x+10][y+1] = 1;
+    world[x+10][y+2] = 1;
+    world[x+11][y-1] = 1;
+    world[x+11][y+3] = 1;
+    world[x+12][y-2] = 1;
+    world[x+12][y+4] = 1;
+    world[x+13][y-2] = 1;
+    world[x+13][y+4] = 1;
+    world[x+14][y+1] = 1;
+    world[x+15][y-1] = 1;
+    world[x+15][y+3] = 1;
+    world[x+16][y] = 1;
+    world[x+16][y+1] = 1;
+    world[x+16][y+2] = 1;
+    world[x+17][y+1] = 1;
+
+    world[x+20][y] = 1;
+    world[x+20][y-1] = 1;
+    world[x+20][y-2] = 1;
+    world[x+21][y] = 1;
+    world[x+21][y-1] = 1;
+    world[x+21][y-2] = 1;
+    world[x+22][y+1] = 1;
+    world[x+22][y-3] = 1;
+    world[x+24][y+1] = 1;
+    world[x+24][y-3] = 1;
+    world[x+24][y+2] = 1;
+    world[x+24][y-4] = 1;
+
+    world[x+34][y-2] = 1;
+    world[x+35][y-2] = 1;
+    world[x+34][y-1] = 1;
+    world[x+35][y-1] = 1;
+  }
+
 }
