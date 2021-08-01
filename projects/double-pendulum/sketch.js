acde3a9acf6c3798a675da481d9536955fa21e69
Francesco Morri <francesco98m@gmail.com>
2021-08-01 18:55:31 +0200

    fixed relative positions of sliders

--- projects/double-pendulum/sketch.js
+++ projects/double-pendulum/sketch.js
@@ -24,32 +24,36 @@ function setup(){
   cv.parent('container');
 
   m1_slider = createSlider(1,100,10,1);
-  m1_slider.position(390, 500);
+  m1_slider.parent('inside-container')
+  m1_slider.position(10,5);
   m1_slider.style('width', '50px');
   m1_label = createElement('p', 'm1');
-  m1_label.position(m1_slider.x + m1_slider.width, 500);
-  m1_label.parent('container');
+  m1_label.position(m1_slider.x + m1_slider.width, m1_slider.y);
+  m1_label.parent('inside-container');
 
   m2_slider = createSlider(1,100,10,1);
-  m2_slider.position(390, 520);
+  m2_slider.parent('inside-container')
+  m2_slider.position(10,25);
   m2_slider.style('width', '50px');
   m2_label = createElement('p', 'm2');
-  m2_label.position(m2_slider.x + m2_slider.width, 520);
-  m2_label.parent('container');
+  m2_label.position(m2_slider.x + m2_slider.width, m2_slider.y);
+  m2_label.parent('inside-container');
 
   l1_slider = createSlider(50,175,100,1);
-  l1_slider.position(390, 540);
+  l1_slider.parent('inside-container')
+  l1_slider.position(10,45);
   l1_slider.style('width', '50px');
   l1_label = createElement('p', 'l1');
-  l1_label.position(l1_slider.x + l1_slider.width, 540);
-  l1_label.parent('container');
+  l1_label.position(l1_slider.x + l1_slider.width, l1_slider.y);
+  l1_label.parent('inside-container');
 
   l2_slider = createSlider(50,175,100,1);
-  l2_slider.position(390, 560);
+  l2_slider.parent('inside-container')
+  l2_slider.position(10,65);
   l2_slider.style('width', '50px');
   l2_label = createElement('p', 'l2');
-  l2_label.position(l2_slider.x + l2_slider.width, 560);
-  l2_label.parent('container');
+  l2_label.position(l2_slider.x + l2_slider.width, l2_slider.y);
+  l2_label.parent('inside-container');
 
   pixelDensity(1);
   cx = width / 2;
